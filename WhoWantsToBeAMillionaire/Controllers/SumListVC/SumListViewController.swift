ef19c4d3bc76d37da514d2fed22dba64747c67c6
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2023-02-10 11:18:03 +0300

    add sumListVC with fix bug of logo
    also add current sums and number of questions

---
+++ WhoWantsToBeAMillionaire/Controllers/SumListVC/SumListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SumListViewController.swift
+//  Millionaire
+//
+//  Created by Eldar Garbuzov on 7.02.23.
+//
+
+import UIKit
+
+class SumListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
